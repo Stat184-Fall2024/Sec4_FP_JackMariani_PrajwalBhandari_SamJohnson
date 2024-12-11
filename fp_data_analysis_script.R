bacee6824ebfa38a70cca8a472336209381af798
Prajwal <pb601027@gmail.com>
2024-12-11 00:25:03 -0500

    still working on data cleaning. will be able to finish and combine all tables very soon. once we get the main table, the analysis should (hopefully) be more straight forward than this. 12/10/24

--- fp_data_analysis_script.R
+++ fp_data_analysis_script.R
@@ -70,15 +70,209 @@ for (year in seq(1980, 2020, by = 4)) {
 }
 
 
-####### data attempt prajwal
-####### working with 2016 data
+### Prajwal data cleaning
 
-# we should write a function for each year, and do something like
+# clean the data from 1980,92,96. This should be pretty straight forward.
+clean_80_92_96 <- function(y){
+  str <- paste0("https://www.presidency.ucsb.edu/statistics/elections/",y)
+  table <- read_html(str) %>%
+    html_element(css = 'table') %>%
+    html_table() %>%
+    slice(-c(1:9)) %>%
+    slice(-c(52:n()))
+  if (y %in% c(1992,1996)) {
 
-# if year == year:
-# function(year)
+    table <- table[, (1:11)] %>%
+      mutate(
+        X5 = ifelse(X5 == "", '0', X5), # replace blanks with 0
+        X8 = ifelse(X8 == "", '0', X8),
+        X11 = ifelse(X11 == "",'0', X11),
+        # convert character cols to numeric by using readr::parse_number
+        X2 = readr::parse_number(X2),
+        X3 = readr::parse_number(X3),
+        X6 = readr::parse_number(X6),
+        X5 = readr::parse_number(X5),
+        X8 = readr::parse_number(X8),
+        X9 = readr::parse_number(X9),
+        X11 = readr::parse_number(X11),
+        X4 = readr::parse_number(gsub("%", "", X4)),
+        X7 = readr::parse_number(gsub("%", "", X7)),
+        X10 = readr::parse_number(gsub("%", "", X10)),
+        year = y # add year col
+      ) %>%
+        rename(
+          state = X1,
+          total_votes = X2,
+          d_votes = X3,
+          d_percent = X4,
+          d_ev = X5,
+          r_votes = X6,
+          r_percent = X7,
+          r_ev = X8,
+          other_votes = X9,
+          other_percent = X10,
+          other_ev = X11,
+          year = year
+        ) %>% # rearrange the order of cols
+        select(
+          year, state, total_votes,
+          d_votes, d_percent, d_ev,
+          r_votes, r_percent, r_ev,
+          other_votes, other_percent, other_ev
+        )
+    }
+  else{
+      table <- table[, (1:11)] %>%
+        mutate(
+          X5 = ifelse(X5 == "", '0', X5), # replace blanks with 0
+          X8 = ifelse(X8 == "", '0', X8),
+          X11 = ifelse(X11 == "",'0', X11),
+          # convert character cols to numeric by using readr::parse_number
+          X2 = readr::parse_number(X2),
+          X3 = readr::parse_number(X3),
+          X6 = readr::parse_number(X6),
+          X5 = readr::parse_number(X5),
+          X8 = readr::parse_number(X8),
+          X9 = readr::parse_number(X9),
+          X11 = readr::parse_number(X11),
+          X4 = readr::parse_number(gsub("%", "", X4)),
+          X7 = readr::parse_number(gsub("%", "", X7)),
+          X10 = readr::parse_number(gsub("%", "", X10)),
+          year = y # add year col
+        ) %>%
+        rename(
+          state = X1,
+          total_votes = X2,
+          r_votes = X3,
+          r_percent = X4,
+          r_ev = X5,
+          d_votes = X6,
+          d_percent = X7,
+          d_ev = X8,
+          other_votes = X9,
+          other_percent = X10,
+          other_ev = X11,
+          year = year
+        ) %>% # rearrange the order of cols
+        select(
+          year, state, total_votes,
+          d_votes, d_percent, d_ev,
+          r_votes, r_percent, r_ev,
+          other_votes, other_percent, other_ev
+        )
+    }
+  return(table)
+}
+# clean the data from 1984, 88
+clean_84_88 <- function(y){
+  str <-  paste0("https://www.presidency.ucsb.edu/statistics/elections/",y)
+  table <- read_html(str) %>%
+    html_element(css = 'table') %>%
+    html_table() %>%
+    slice(-(1:10)) %>%
+    slice(-(52:n()))
 
+  table <- table[, (1:11)] %>%
+    mutate(
+      X5 = ifelse(X5 == "", '0', X5), # replace blanks with 0
+      X8 = ifelse(X8 == "", '0', X8),
+      X11 = ifelse(X11 == "",'0', X11),
+      # convert character cols to numeric by using readr::parse_number
+      X2 = readr::parse_number(X2),
+      X3 = readr::parse_number(X3),
+      X6 = readr::parse_number(X6),
+      X5 = readr::parse_number(X5),
+      X8 = readr::parse_number(X8),
+      X4 = readr::parse_number(gsub("%", "", X4)),
+      X7 = readr::parse_number(gsub("%", "", X7)),
+      X9 = X2 - X3 - X6, # other votes = total - d - r
+      X11 = 0, #third parties don't get any Electoral Votes
+      year = y # add year col
+    ) %>%
+    mutate(
+      X10 = 100 - X4 - X7 #mutate again for percentage, can use X4 and X7 since they are numeric
+    ) %>%
+    rename(
+      state = X1,
+      total_votes = X2,
+      r_votes = X3,
+      r_percent = X4,
+      r_ev = X5,
+      d_votes = X6,
+      d_percent = X7,
+      d_ev = X8,
+      other_votes = X9,
+      other_percent = X10,
+      other_ev = X11,
+      year = year
+    ) %>% # rearrange the order of cols
+    select(
+      year, state, total_votes,
+      d_votes, d_percent, d_ev,
+      r_votes, r_percent, r_ev,
+      other_votes, other_percent, other_ev
+    )
+  if (y == 1988) {
+    table[49,12] <- 1 # a faithless elector gave a vote to Lloyd Bernstein
+    table[49,2] <- "West Virginia"
+  }
 
+  return(table)
+}
+# clean the data from 2000
+clean_2000 <- function(){
+  str <-  paste0("https://www.presidency.ucsb.edu/statistics/elections/2000")
+  table <- read_html(str) %>%
+    html_element(css = 'table') %>%
+    html_table() %>%
+    slice(-(1:9)) %>%
+    slice(-(52:n()))
+  table <- table[, (1:11)] %>%
+    mutate(
+      X5 = ifelse(X5 == "", '0', X5), # replace blanks with 0
+      X8 = ifelse(X8 == "", '0', X8),
+      X11 = ifelse(X11 == "",'0', X11),
+      # convert character cols to numeric by using readr::parse_number
+      X2 = readr::parse_number(X2),
+      X3 = readr::parse_number(X3),
+      X6 = readr::parse_number(X6),
+      X5 = readr::parse_number(X5),
+      X8 = readr::parse_number(X8),
+      X9 = X2 - X3 - X6, # other votes = total - d - r
+      X11 = 0, #third parties dont get any Electoral Votes
+      X4 = readr::parse_number(gsub("%", "", X4)),
+      X7 = readr::parse_number(gsub("%", "", X7)),
+      year = 2000 # add year col
+    ) %>%
+    rename(
+      state = X1,
+      total_votes = X2,
+      r_votes = X3,
+      r_percent = X4,
+      r_ev = X5,
+      d_votes = X6,
+      d_percent = X7,
+      d_ev = X8,
+      other_votes = X9,
+      other_percent = X10,
+      other_ev = X11,
+      year = year
+    ) %>% # rearrange the order of cols
+    select(
+      year, state, total_votes,
+      d_votes, d_percent, d_ev,
+      r_votes, r_percent, r_ev,
+      other_votes, other_percent, other_ev
+    )
+  table[9,2] <- 'Dist. of Col.' #account for asterisk. We mention this in the report
+
+  return(table)
+}
+# clean the data from 2004
+clean_2004 <- function(){
+  # to do
+}
+# clean the data from 2016
 clean_2016 <- function(){
   str <- "https://www.presidency.ucsb.edu/statistics/elections/2016"
   table <- read_html(str) %>%
@@ -139,21 +333,6 @@ clean_2016 <- function(){
   return(table)
 }
 
-# clean the data from 1980. This should be pretty straight forward.
-
-clean_80_92_96 <- function(y){
-  str <- paste0("https://www.presidency.ucsb.edu/statistics/elections/",y)
-  table <- read_html(str) %>%
-    html_element(css = 'table') %>%
-    html_table() %>%
-    slice(-c(1:9)) %>%
-    slice(-c(52:n()))
-  table <- table[, (1:11)]
-
-  return(table)
-}
-
-
 # actual function to create the table:
 create_clean_data <- function(){
 
@@ -170,9 +349,17 @@ create_clean_data <- function(){
       html_table()
     rbind(clean_data, clean_this_table(table))
 
+  #1980, 1992,1996
+  if (year %in% c(1980,1992,1996)){
+    rbind(clean_data, clean_80_92_96(year))
+  }
+  # 1884
+  if (year %in% c(1984,1988)) {
+    rbind(clean_data, clean_84_88(year))
+  }
 
   # 2016
-  if(year == 2016) {
+  if (year == 2016) {
       rbind(clean_data, clean_2016())
     }
   }
